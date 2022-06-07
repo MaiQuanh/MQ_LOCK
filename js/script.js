01c9f462db81fd5a3932f12174526e443afbf4d8
MaiQuanh <mquanh98@gmail.com>
2022-06-07 11:37:11 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,129 @@
+// ---------------------MENU----------------------
+
+const Top = document.querySelector(".top")
+window.addEventListener("scroll", function () {
+    const X = window.pageYOffset;
+    if (X > 1) {
+        Top.classList.add("active")
+    } else {
+        Top.classList.remove("active")
+    }
+
+})
+
+// ---------------CARTEGORY------------
+
+const itemssliderbar = document.querySelectorAll(".cartegory-left-li")
+itemssliderbar.forEach(function (menu, index) {
+    menu.addEventListener("click", function () {
+        menu.classList.toggle("block")
+    })
+})
+
+
+// ----------------------PRODUCT----------------
+
+// Trinh chieu anh to va nho
+const bigImg = document.querySelector(".product-content-left-big-img img")
+const smalImg = document.querySelectorAll(".product-content-left-small-img img")
+smalImg.forEach(function (imgItem, X) {
+    imgItem.addEventListener("click", function () {
+        bigImg.src = imgItem.src
+    })
+})
+
+// Hien thi noi dung
+const baohanh = document.querySelector(".baohanh")
+const chitiet = document.querySelector(".chitiet")
+if (baohanh) {
+    baohanh.addEventListener("click", function () {
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "none"
+        document.querySelector(".product-content-right-bottom-content-baohanh").style.display = "block"
+    })
+}
+
+if (chitiet) {
+    chitiet.addEventListener("click", function () {
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "block"
+        document.querySelector(".product-content-right-bottom-content-baohanh").style.display = "none"
+    })
+}
+
+const buttons = document.querySelector(".product-content-right-bottom-top")
+if (buttons) {
+    buttons.addEventListener("click", function () {
+        document.querySelector(".product-content-right-bottom-content-big").classList.toggle("nactive")
+    })
+}
+
+// ----------------------DELIVERY----------------------
+
+
+if (address_2 = localStorage.getItem('address_2_saved')) {
+  $('select[name="calc_shipping_district"] option').each(function() {
+    if ($(this).text() == address_2) {
+      $(this).attr('selected', '')
+    }
+  })
+  $('input.billing_address_2').attr('value', address_2)
+}
+if (district = localStorage.getItem('district')) {
+  $('select[name="calc_shipping_district"]').html(district)
+  $('select[name="calc_shipping_district"]').on('change', function() {
+    var target = $(this).children('option:selected')
+    target.attr('selected', '')
+    $('select[name="calc_shipping_district"] option').not(target).removeAttr('selected')
+    address_2 = target.text()
+    $('input.billing_address_2').attr('value', address_2)
+    district = $('select[name="calc_shipping_district"]').html()
+    localStorage.setItem('district', district)
+    localStorage.setItem('address_2_saved', address_2)
+  })
+}
+$('select[name="calc_shipping_provinces"]').each(function() {
+  var $this = $(this),
+    stc = ''
+  c.forEach(function(i, e) {
+    e += +1
+    stc += '<option value=' + e + '>' + i + '</option>'
+    $this.html('<option value="">Tỉnh / Thành phố</option>' + stc)
+    if (address_1 = localStorage.getItem('address_1_saved')) {
+      $('select[name="calc_shipping_provinces"] option').each(function() {
+        if ($(this).text() == address_1) {
+          $(this).attr('selected', '')
+        }
+      })
+      $('input.billing_address_1').attr('value', address_1)
+    }
+    $this.on('change', function(i) {
+      i = $this.children('option:selected').index() - 1
+      var str = '',
+        r = $this.val()
+      if (r != '') {
+        arr[i].forEach(function(el) {
+          str += '<option value="' + el + '">' + el + '</option>'
+          $('select[name="calc_shipping_district"]').html('<option value="">Quận / Huyện</option>' + str)
+        })
+        var address_1 = $this.children('option:selected').text()
+        var district = $('select[name="calc_shipping_district"]').html()
+        localStorage.setItem('address_1_saved', address_1)
+        localStorage.setItem('district', district)
+        $('select[name="calc_shipping_district"]').on('change', function() {
+          var target = $(this).children('option:selected')
+          target.attr('selected', '')
+          $('select[name="calc_shipping_district"] option').not(target).removeAttr('selected')
+          var address_2 = target.text()
+          $('input.billing_address_2').attr('value', address_2)
+          district = $('select[name="calc_shipping_district"]').html()
+          localStorage.setItem('district', district)
+          localStorage.setItem('address_2_saved', address_2)
+        })
+      } else {
+        $('select[name="calc_shipping_district"]').html('<option value="">Quận / Huyện</option>')
+        district = $('select[name="calc_shipping_district"]').html()
+        localStorage.setItem('district', district)
+        localStorage.removeItem('address_1_saved', address_1)
+      }
+    })
+  })
+})
---
+++ js/slide.js
@@ -0,0 +1,34 @@
+// Slider
+const imgPosition = document.querySelectorAll(".aspect-ratio-169 img")
+const imgContainer = document.querySelector(".aspect-ratio-169")
+const dotItem = document.querySelectorAll(".dot")
+let imgNuber = imgPosition.length
+let index = 0
+// console.log(imgPosition)
+imgPosition.forEach(function (image, index) {
+    image.style.left = index * 100 + "%"
+    dotItem[index].addEventListener("click", function () {
+        slider(index)
+
+    })
+})
+
+function imgSlide() {
+    index++;
+    console.log(index)
+    if (index >= imgNuber) {
+        index = 0
+    }
+    slider(index)
+
+}
+
+function slider(index) {
+    imgContainer.style.left = "-" + index * 100 + "%"
+    const dotActive = document.querySelector(".actives")
+    dotActive.classList.remove("actives")
+    dotItem[index].classList.add("actives")
+
+
+}
+setInterval(imgSlide, 5000)
